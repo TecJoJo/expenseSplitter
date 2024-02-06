4b9e18e71848064845549452e47bc0d1ae629734
Yao Lu Home Win11 <luyao2493751@gmail.com>
2024-02-07 00:51:10 +0200

    add person button added

--- expense-splitter/src/component/Form.tsx
+++ expense-splitter/src/component/Form.tsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { ReactNode, useState } from "react";
+import styled from "styled-components";
 
 function Form() {
+  const [personAmount,setPersonAmount] = useState(1)
+
   const InputContainer = styled.div`
     display: flex;
     flex-direction: row;
     margin: 1rem 0;
   `;
   const LabelWrapper = styled.div`
-    border: solid 1px black;
-    border-radius:10px 0 0 10px;
-  `
-  const Label = styled.label`
+    border-left: solid 1px black;
+    border-top: solid 1px black;
+    border-bottom: solid 1px black;
 
+    border-radius: 10px 0 0 10px;
+    min-width: 6rem;
   `;
+  const Label = styled.label``;
   const Input = styled.input`
-    width:30rem;
+    width: 30rem;
     border: solid 1px black;
     border-radius: 0 10px 10px 0;
   `;
 
-  interface GroupedInputProps {
-    name: string;
-    placeholder: string;
-  }
-
-  const GroupedInput:React.FC<GroupedInputProps> = ({name,placeholder}) => {
+  const GroupedInput: React.FC<GroupedInputProps> = ({ name, placeholder }) => {
     return (
       <InputContainer>
         <LabelWrapper>
@@ -37,22 +36,47 @@ function Form() {
     );
   };
 
+  interface GroupedInputProps {
+    name: string;
+    placeholder: string;
+  }
+  const ParticipantForm: React.FC = () => {
+    return (
+      <>
+        <GroupedInput name={"NAME"} placeholder={"Your Name"} />
+        <GroupedInput name={"Payment"} placeholder={"Payment in advance"} />
+      </>
+    );
+  };
+
+  
+  // let persons:JSX.Element[] = []
+  // for(let i = 0;i<personAmount;i++){
+  //   persons.push(<ParticipantForm/>)
+  // }
+
+      
+    
+  
+
+  const addPerson:React.MouseEventHandler<HTMLButtonElement> = (e)=>{
+    e.preventDefault()
+    setPersonAmount((number)=>number+=1)
+
+  }
+
+
+
   return (
-    <form style={{marginBottom:"20vh"}} action="process_payment.php" method="post">
-      <GroupedInput name={"NAME"} placeholder={"Your Name"}/>
-      <GroupedInput name={"Payment"} placeholder={"Payment in advance"}/>
-      <GroupedInput name={"NAME"} placeholder={"Your Name"}/>
-      <GroupedInput name={"Payment"} placeholder={"Payment in advance"}/>
-      <GroupedInput name={"NAME"} placeholder={"Your Name"}/>
-      <GroupedInput name={"Payment"} placeholder={"Payment in advance"}/>
-      <GroupedInput name={"NAME"} placeholder={"Your Name"}/>
-      <GroupedInput name={"Payment"} placeholder={"Payment in advance"}/>
-      <GroupedInput name={"NAME"} placeholder={"Your Name"}/>
-      <GroupedInput name={"Payment"} placeholder={"Payment in advance"}/>
+    <form action="" method="">
       
+      {Array.from({length:personAmount}).map((person,index)=><ParticipantForm key={index}/>)}
       
+      <button onClick={addPerson} className=" btn btn-primary ">
+        add person
+      </button>
     </form>
   );
 }
 
-export default Form
+export default Form;
